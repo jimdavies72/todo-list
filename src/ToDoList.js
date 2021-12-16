7e41294cff7f5a45e3e8f3b3157bc19b7852b222
James Davies <60436624+jimdavies72@users.noreply.github.com>
2021-12-16 10:46:28 +0000

    styled. unable to get update item to work.

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -4,26 +4,42 @@ import { useState } from "react";
 const ToDoList = () => {
   const [toDoItems, setToDoItems] = useState([]);
   const [toDoText, setToDoText] = useState("");
+  const [toggle, setToggle] = useState(true);
 
   const inputTextHandler = (event) => {
     setToDoText(event.target.value);
   };
 
+  const KeyDownHandler = (event) => {
+    if (event.key === "Enter") {
+      addHandler();
+    }
+  };
+
   const addHandler = () => {
-    const newArray = [...toDoItems];
-    const itemObject = {
-      text: toDoText,
-      complete: false,
-    };
-    newArray.push(itemObject);
-    setToDoItems(newArray);
-    setToDoText("");
+    if (toDoText !== "") {
+      const newArray = [...toDoItems];
+      const itemObject = {
+        text: toDoText,
+        complete: false,
+      };
+      newArray.push(itemObject);
+      setToDoItems(newArray);
+      setToDoText("");
+    }
   };
 
   const removeHandler = (index) => {
     const newArray = [...toDoItems];
     newArray.splice(index, 1);
     setToDoItems(newArray);
+  };
+
+  const editHandler = (index) => {
+    //window.alert("hello");
+    {
+      toggle ? setToggle(false) : <input type="text" value="" />;
+    }
   };
 
   const tickHandler = (index, complete) => {
@@ -35,10 +51,21 @@ const ToDoList = () => {
   return (
     <div className="container">
       <div className="todo-list">
-        <h1>To Do List</h1>
-        <div>
-          <input type="text" value={toDoText} onChange={inputTextHandler} />
-          <i className="fa fa-plus-square" onClick={addHandler}></i>
+        <span className="heading">
+          <h1>To Do List</h1>
+        </span>
+        <div className="input-fields">
+          <input
+            type="text"
+            className="text-input"
+            placeholder="Item"
+            value={toDoText}
+            onChange={inputTextHandler}
+            onKeyDown={KeyDownHandler}
+          />
+          <p className="add-item">
+            <i className="fa fa-plus-square" onClick={addHandler}></i>
+          </p>
         </div>
         {toDoItems.map((toDoItem, index) => {
           return (
@@ -48,6 +75,7 @@ const ToDoList = () => {
               complete={toDoItem.complete}
               removeMe={() => removeHandler(index)}
               tickMe={() => tickHandler(index, toDoItem.complete)}
+              editMe={() => editHandler(index)}
             />
           );
         })}
@@ -58,18 +86,26 @@ const ToDoList = () => {
 
 const ItemCard = (props) => {
   return (
-    <div className="itemCard">
+    <div className="item-card">
       <span className={props.complete ? "complete" : ""}>
-        <h3>{props.item}</h3>
+        <h3 className="item" onDoubleClick={props.editMe}>
+          {props.item}
+        </h3>
       </span>
-      <>
+      <div className="item-icons">
         {props.complete ? (
-          <i className="fa fa-times" onClick={props.tickMe}></i>
+          <p>
+            <i className="fa fa-times" onClick={props.tickMe}></i>
+          </p>
         ) : (
-          <i className="fa fa-check" onClick={props.tickMe}></i>
+          <p>
+            <i className="fa fa-check" onClick={props.tickMe}></i>
+          </p>
         )}
-        <i className="fa fa-trash" onClick={props.removeMe}></i>
-      </>
+        <p>
+          <i className="fa fa-trash" onClick={props.removeMe}></i>
+        </p>
+      </div>
     </div>
   );
 };
