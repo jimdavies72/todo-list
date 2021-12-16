51c94083ca355bc70ebbbbb05dd969053af0cf94
James Davies <60436624+jimdavies72@users.noreply.github.com>
2021-12-16 14:08:08 +0000

    Final version

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "font-awesome/css/font-awesome.min.css";
 
-//import ToDoList from "./ToDoList";
-import Calculator from "./Calculator";
+import ToDoList from "./ToDoList";
+//import Calculator from "./Calculator";
 
-//ReactDOM.render(<ToDoList />, document.getElementById("root"));
-ReactDOM.render(<Calculator />, document.getElementById("root"));
+ReactDOM.render(<ToDoList />, document.getElementById("root"));
+//ReactDOM.render(<Calculator />, document.getElementById("root"));
