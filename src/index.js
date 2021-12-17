b572b530c6f788fe883dcb66f85ca0762531c92d
James Davies <60436624+jimdavies72@users.noreply.github.com>
2021-12-17 10:13:04 +0000

    deleted old calc work.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import "./index.css";
 import "font-awesome/css/font-awesome.min.css";
 
 import ToDoList from "./ToDoList";
-//import Calculator from "./Calculator";
 
 ReactDOM.render(<ToDoList />, document.getElementById("root"));
-//ReactDOM.render(<Calculator />, document.getElementById("root"));
